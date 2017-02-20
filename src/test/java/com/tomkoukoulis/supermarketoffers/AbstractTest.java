e1239a9c2a3e656d01ace2c911f712b02e53bcf2
tomkoukoulis <tomkoukoulis@toms-imac.lan>
2017-02-20 17:31:41 +0000

    Corrected typo in AbstractTest class

--- src/test/java/com/tomkoukoulis/supermarketoffers/AbstractTest.java
+++ src/test/java/com/tomkoukoulis/supermarketoffers/AbstractTest.java
@@ -20,7 +20,7 @@ public class AbstractTest {
     public AbstractTest() {
         testItems = new HashMap<>();
         testItems.put("Pizza Margherita", new Item("Pizza Express Margherita 245g", 450));
-        testItems.put("OrganiisOnOfferives", new Item("Real Olive Co Organic Kalamata 210g", 299));
+        testItems.put("Organic Olives", new Item("Real Olive Co Organic Kalamata 210g", 299));
         testItems.put("Key Lime Pies", new Item("GU Key Lime Pies 2x85g", 300));
         testItems.put("Free Range Chicken", new Item("Free Range Chicken Drumsticks 412g", 579));
         testItems.put("Organic New Potatoes", new Item("Organic New Potatoes 500g", 169));
