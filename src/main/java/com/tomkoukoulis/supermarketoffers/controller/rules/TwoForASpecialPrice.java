31176afe1ccb81c1037cf4b2bc2b2e1a5aac273f
tomkoukoulis <tomkoukoulis@toms-imac.lan>
2017-02-20 20:01:05 +0000

    Added TwoForASpecialPrice test and class

---
+++ src/main/java/com/tomkoukoulis/supermarketoffers/controller/rules/TwoForASpecialPrice.java
@@ -0,0 +1,58 @@
+package com.tomkoukoulis.supermarketoffers.controller.rules;
+
+import com.tomkoukoulis.supermarketoffers.model.Item;
+import com.tomkoukoulis.supermarketoffers.model.Receipt;
+
+/**
+ *
+ * @author tomkoukoulis
+ */
+public class TwoForASpecialPrice implements Rule {
+    
+    /**
+     * The number of items of this offer
+     */
+    protected static final int NO_OF_ITEMS = 2;
+    
+    /**
+     * The special offer price
+     */
+    protected int specialPrice;
+
+    /**
+     * The discount to match the offer
+     */
+    protected int discount;
+
+    /**
+     * The item on offer
+     */
+    protected Item item;
+
+    /**
+     * Constructor
+     *  
+     * @param item the item on offer
+     * @param specialPrice the offer price
+     */
+    public TwoForASpecialPrice(Item item, int specialPrice) {
+        this.item = item;
+        this.specialPrice = specialPrice;
+        this.discount = specialPrice - item.getPrice()*2;
+    }    
+//       
+//    public Item getItem() {
+//        return this.item;
+//    }
+
+    @Override
+    public Item addOffer() {
+        return new Item("\tBuy 2 for £" + String.format("%.2f", (float) specialPrice/100), discount);    
+    }
+    
+    @Override
+    public boolean checkPrerequisites(Receipt receipt) {
+        int occurrences = receipt.getFrequency(item);
+        return (occurrences % NO_OF_ITEMS) == 0;
+    }
+}
---
+++ src/test/java/com/tomkoukoulis/supermarketoffers/controller/rules/TwoForASpecialPriceTest.java
@@ -0,0 +1,83 @@
+package com.tomkoukoulis.supermarketoffers.controller.rules;
+
+import com.tomkoukoulis.supermarketoffers.AbstractTest;
+import com.tomkoukoulis.supermarketoffers.model.Item;
+import com.tomkoukoulis.supermarketoffers.model.Receipt;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author tomkoukoulis
+ */
+public class TwoForASpecialPriceTest extends AbstractTest {
+    
+    /**
+     * Constructor
+     */
+    public TwoForASpecialPriceTest() {
+    }
+
+    /**
+     * Test of addOffer method, of class TwoForASpecialPrice.
+     */
+    @Test
+    public void testAddOffer() {
+        System.out.println("addOffer");
+        
+        Item pizza = testItems.get("Pizza Margherita");
+        
+        int specialPrice = 600;
+        int discount = specialPrice - pizza.getPrice()*2;
+        
+        TwoForASpecialPrice offer = new TwoForASpecialPrice(pizza, specialPrice);
+        
+        Item expResult = new Item("\tBuy 2 for £" + String.format("%.2f", (float) specialPrice/100), discount);
+        Item result = offer.addOffer();
+        
+        System.out.println("Offer: " + result.getDescription());
+        
+        assertEquals(expResult.getDescription(), result.getDescription());
+        assertEquals(expResult.getPrice(), result.getPrice());
+    }
+
+    /**
+     * Test of checkPrerequisites method, of class TwoForASpecialPrice.
+     */
+    @Test
+    public void testCheckPrerequisitesTrue() {
+        System.out.println("checkPrerequisites True");
+       
+        int specialPrice = 600;
+        
+        TwoForASpecialPrice offer = new TwoForASpecialPrice(testItems.get("Pizza Margherita"), specialPrice);
+        
+        Receipt receipt = new Receipt();
+        receipt.add(testItems.get("Pizza Margherita"));
+        receipt.add(testItems.get("Pizza Margherita"));
+        
+        boolean result = offer.checkPrerequisites(receipt);
+        assertTrue(result);
+    }
+
+    /**
+     * Test of checkPrerequisites method, of class TwoForASpecialPrice.
+     */
+    @Test
+    public void testCheckPrerequisitesFalse() {
+        System.out.println("checkPrerequisites False");
+       
+        int specialPrice = 600;
+        
+        TwoForASpecialPrice offer = new TwoForASpecialPrice(testItems.get("Pizza Margherita"), specialPrice);
+        
+        Receipt receipt = new Receipt();
+        receipt.add(testItems.get("Organic Olives"));
+        receipt.add(testItems.get("Organic Olives"));
+        receipt.add(testItems.get("Pizza Margherita"));
+        
+        boolean result = offer.checkPrerequisites(receipt);
+        
+        assertFalse(result);
+    }    
+}
